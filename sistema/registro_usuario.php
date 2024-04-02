e020f5abee2e3172658639b1db68b5c8433e4c54
INOXBEAT <jpreciado85@gmail.com>
2024-04-01 22:31:04 -0500

    cambio método  a  y agregado de mensajes de error en el registro de usuarios

--- sistema/registro_usuario.php
+++ sistema/registro_usuario.php
@@ -1,52 +1,50 @@
 <?php
-    include "../conexion.php";
-    if(!empty($_post))
-    {
-           $alert='';
-           if(empty($_post['nombre'])||empty($_post['correo'])||empty($_post['usuario'])||empty($_post['clave'])||empty($_post['rol']))
-           {
-            $alert='<p class="msg_error">Todos los campos son obligatorios.</p>';
-           }else{
+include "../conexion.php";
+if (!empty($_POST)) {
+    $alert = '';
+    if (empty($_POST['nombre']) || empty($_POST['correo']) || empty($_POST['usuario']) || empty($_POST['clave']) || empty($_POST['rol'])) {
+        $alert = '<p class="msg_error">Todos los campos son obligatorios.</p>';
+    } else {
+        $nombre = $_POST['nombre'];
+        $email = $_POST['correo'];
+        $user = $_POST['usuario'];
+        $clave = md5($_POST['clave']);
+        $rol = $_POST['rol'];
 
-                $nombre = $_post['nombre'];
-                $email = $_post['correo'];
-                $user = $_post['usuario'];
-                $clave = md5($_post['clave']);
-                $rol = $_post['rol'];
+        $query = mysqli_query($conection, "SELECT * FROM usuario WHERE usuario = '$user' OR correo = '$email'");
+        $result = mysqli_fetch_array($query);
 
-                $query = mysqli_query($conection,"SELECT * FROM usuario WHERE usuario = '$user' OR correo = '$email' ");
-                $result = mysqli_fetch_array($query);
+        if ($result > 0) {
+            $alert = '<p class="msg_error">El usuario o el correo ya existe.</p> ';
+        } else {
+            $query_insert = mysqli_query($conection, "INSERT INTO usuario(nombre,correo,usuario,clave,rol) VALUES('$nombre','$email','$user','$clave','$rol')");
 
-                if($result > 0){
-                    $alert= '<p class="msg_error">El usuario o el correo ya existe.</p> ';    
-                }else{
-                    $query_insert=mysqli_query($conection,"INSERT INTO usuario(nombre,correo,usuario,clave,rol)
-                        VALUES('$nombre','$email','$user','$clave','$rol')");
-                
-                if($query_insert){
-                    $alert='<p class="msg_save">Usuario creado exitosamente.</p> '; 
-                }else{
-                    $alert='<p class="msg_error">Error al crear el usuario.</p> ';
-                }
+            if ($query_insert) {
+                $alert = '<p class="msg_save">Usuario creado exitosamente.</p>';
+            } else {
+                $alert = '<p class="msg_error">Error al crear el usuario: ' . mysqli_error($conection) . '</p>';
             }
-        }    
-    }               
+        }
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<?php include"includes/scripts.php";?>
-	<title>Registro Usuario</title>
+    <meta charset="UTF-8">
+    <?php include "includes/scripts.php"; ?>
+    <title>Registro Usuario</title>
 </head>
+
 <body>
-	<?php include"includes/header.php";?>
-	<section id="container">
+    <?php include "includes/header.php"; ?>
+    <section id="container">
         <div class="form_register">
             <h1>Registro usuario</h1>
             <hr>
-            <div class="alert"><?php echo isset($alert) ? $alert: ''; ?></div>
-            <form action="" method="poss">
+            <div class="alert"><?php echo isset($alert) ? $alert : ''; ?></div>
+            <form action="" method="post">
                 <label for="nombre">Nombre</label>
                 <input type="text" name="nombre" id="nombre" placeholder="Nombre completo">
                 <label for="correo">Correo electronico</label>
@@ -56,29 +54,28 @@
                 <label for="clave">Clave</label>
                 <input type="password" name="clave" id="clave" placeholder="Contraseña">
                 <label for="rol">Tipo de usuario</label>
-            <?php
-                $query_rol=mysqli_query($conection,"SELECT * FROM rol");
-                $result_rol=mysqli_num_rows($query_rol); 
-            ?>
+                <?php
+                $query_rol = mysqli_query($conection, "SELECT * FROM rol");
+                $result_rol = mysqli_num_rows($query_rol);
+                ?>
                 <select name="rol" id="rol">
-            <?php 
-                if($result_rol > 0)
-                {
-                while($rol=mysqli_fetch_array($query_rol)){
-            ?>        
-                <option value="<?php echo $rol ["idrol"]; ?>"><?php echo $rol["rol"] ?></option>
-            <?php    
-                        # code...
-                   }     
-                }                      
-            ?>        
+                    <?php
+                    if ($result_rol > 0) {
+                        while ($rol = mysqli_fetch_array($query_rol)) {
+                    ?>
+                            <option value="<?php echo $rol["idrol"]; ?>"><?php echo $rol["rol"] ?></option>
+                    <?php
+                        }
+                    }
+                    ?>
                 </select>
-                <input type="submit" value="Crear usuario" class="btn_save">                                             
+                <input type="submit" value="Crear usuario" class="btn_save">
             </form>
         </div>
-	</section>
+    </section>
 
-	<?php include"includes/footer.php";?>
+    <?php include "includes/footer.php"; ?>
 
 </body>
+
 </html>
